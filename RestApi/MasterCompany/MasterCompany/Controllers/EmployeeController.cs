248ecf90a07e2dbf1196a623e4763427602fd960
Julio Cesar Tapia Moya <julio.tapiamoya@gmail.com>
2023-01-21 12:34:34 -0400

    -Employee model added. -EmployeeController added. -The file with the list of employees was added (ActiveEmployees.json).

---
+++ RestApi/MasterCompany/MasterCompany/Controllers/EmployeeController.cs
@@ -0,0 +1,42 @@
+﻿using MasterCompany.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Linq;
+using System.Drawing;
+
+
+namespace MasterCompany.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        static string fileName = "./Sources/ActiveEmployees.json";
+        static List<Employee> employees = new();
+
+        [HttpGet]
+        public async Task<List<Employee>> GetAllEmployees()
+        {
+            await LoadJsonFile();
+            return employees;
+        }
+
+        #region LoadAndSave
+        public async Task<int> LoadJsonFile()
+        {
+            await using FileStream openStream = System.IO.File.OpenRead(fileName);
+            employees = await JsonSerializer.DeserializeAsync<List<Employee>>(openStream);
+            return 0;
+        }
+
+        public async Task<int> SaveJsonFile()
+        {
+            using FileStream createStream = System.IO.File.Create(fileName);
+            await JsonSerializer.SerializeAsync(createStream, employees);
+            await createStream.DisposeAsync();
+            return 0;
+        }
+        #endregion
+    }
+}
---
+++ RestApi/MasterCompany/MasterCompany/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace MasterCompany.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Document { get; set; }
+        public long Salary { get; set; }
+        public string Gender { get; set; }
+        public string Position { get; set; }
+        public string StartDate { get; set; }
+    }
+}
